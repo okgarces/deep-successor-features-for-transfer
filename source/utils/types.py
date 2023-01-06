88978477688bf35bd3f9c39a8b2583e0876e9885
Oscar Kiyoshige Garces Aparicio <dkodness@gmail.com>
2023-01-06 18:31:21 +1100

    Fix type. Allow Any

--- source/utils/types.py
+++ source/utils/types.py
@@ -1,6 +1,3 @@
-from torch.nn.modules.loss import _Loss as Loss
-from torch.optim import Optimizer
-from torch.nn import Module
-from typing import Tuple
+from typing import Any
 
-ModelTuple = Tuple[Module, Loss, Optimizer|None]
+ModelTuple = Any
