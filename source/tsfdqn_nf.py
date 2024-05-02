b55072520b71d8b9f6a6c8f8a295034f054605e1
Oscar Kiyoshige Garces Aparicio <dkodness@gmail.com>
2024-05-02 15:48:53 +1000

    No grad to next target tsf. Something went wrong with the omegas

--- source/tsfdqn_nf.py
+++ source/tsfdqn_nf.py
@@ -1277,10 +1277,15 @@ class TSFDQN:
         next_successor_features = self.sf.get_next_successors(s1_torch).detach()
         r_tensor = torch.tensor(r).float().unsqueeze(0).to(self.device).detach()
 
-        next_target_tsf = torch.sum(next_successor_features * omegas, axis=1)[:, a1, :]
-        next_q_value = r_tensor + (1 - float(done)) * self.gamma * w_approx(next_target_tsf).reshape(-1)
+        # TODO Remove this. This is to double check that omegas are not being learnt as that suppose to do.
+        with torch.no_grad():
+            next_target_tsf = torch.sum(next_successor_features * omegas, axis=1)[:, a1, :]
+            next_q_value = r_tensor + (1 - float(done)) * self.gamma * w_approx(next_target_tsf).reshape(-1)
 
+        # TODO Remove this. Weights are not being learnt properly.
         r_fit = w_approx(transformed_phi).reshape(-1)
+        # r_fit = w_approx(phi_tensor).reshape(-1)
+
         next_tsf = transformed_phi + (1 - float(done)) * self.gamma * next_target_tsf
 
         tsf = torch.sum(successor_features * omegas, axis=1)[:, a ,:]
